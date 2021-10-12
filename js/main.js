9dca55605681ba266b173f680d3b886888637114
Алексей Панфилов <panfav40@gmail.com>
2021-10-13 02:48:07 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function () {
     $('.slider__box').slick()
-    $('.case__slider-box')slick({
+    $('.case__slider-box').slick({
         prevArrow: $('.case__prev-button'),
         nextArrow: $('.case__next-button')
     })
