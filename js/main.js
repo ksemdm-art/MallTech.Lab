95295105410ba0496b41ae09dfdf1c1658dc9ada
Trotskiy12 <mebelend33@gmail.com>
2021-10-13 04:15:27 +0300

    header adaptive and scroll menu

--- js/main.js
+++ js/main.js
@@ -5,3 +5,30 @@ $(function () {
         nextArrow: $('.case__next-button')
     })
 });
+
+
+$(document).ready(function() {
+
+    var element = $(".buttons");
+    var height_el = element.offset().top;
+    
+    $(".buttons").css({
+        "width": element.outerWidth(),
+        "height": element.outerHeight()
+    });
+
+    $(window).scroll(function() {
+        
+        if($(window).scrollTop() > height_el) {
+        
+            element.addClass("fixed");
+
+        } else {
+        
+            element.removeClass("fixed");
+
+        }
+
+    });
+
+});
